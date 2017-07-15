04a2497a3ce203eb2fbc3844d0233188f6c91da9
YasharRahvar <Yasharrah909@gmail.com>
2017-07-15 01:00:06 -0700

    Project converted to JPA and employee class annotated.

--- ejb_project/src/main/java/ejb_project/entity/Employee.java
+++ ejb_project/src/main/java/ejb_project/entity/Employee.java
@@ -8,10 +8,50 @@
 
 package ejb_project.entity;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
 /**
  * @author Yashar Rahvar
  *
  */
+@Entity
+@Table
 public class Employee {
 
+	@Id
+	private String id;
+	@Column
+	private String firstName;
+	@Column
+	private String lastName;
+	@Column
+	private String dob;
+	
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public String getFirstName() {
+		return firstName;
+	}
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	public String getLastName() {
+		return lastName;
+	}
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	public String getDob() {
+		return dob;
+	}
+	public void setDob(String dob) {
+		this.dob = dob;
+	}
 }
