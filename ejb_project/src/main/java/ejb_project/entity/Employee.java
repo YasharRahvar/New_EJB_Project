0b792487613047ec825154a8cb34b86b9cd5a3bd
YasharRahvar <Yasharrah909@gmail.com>
2017-07-15 00:35:52 -0700

    Created employee class.

---
+++ ejb_project/src/main/java/ejb_project/entity/Employee.java
@@ -0,0 +1,17 @@
+/**
+ * Project: ejb_project
+ * File: Employee.java
+ * Date: Jul 15, 2017
+ * Time: 12:32:40 AM
+ * Description: This class is a entity class which represent a employee table in database. 
+ */
+
+package ejb_project.entity;
+
+/**
+ * @author Yashar Rahvar
+ *
+ */
+public class Employee {
+
+}
